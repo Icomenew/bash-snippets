ce9bac0ad0d63ec6c5923a485e3b9e50f5b15925
auspbro <auspbro@gmail.com>
2019-09-27 14:34:48 +0800

    Add copy package feature

--- sw_data_capture.sh
+++ sw_data_capture.sh
@@ -3,9 +3,8 @@
 # mail: auspbro@gmail.com
 
 
-work_week=$1
-platform=$2
-driver_build=$3
+driver_build=$1
+gta_pac_path="C:/Users/xuex1x/Downloads/"   # GTA package download path on local
 
 # help info function
 help_info() {
@@ -15,23 +14,32 @@ help_info() {
 	echo -e "Args\t\t Describe"
 	echo -e "  -h\t\t get all help infomations"
 	echo -e "eg:"
-	echo -e "$0 work_week platform driver_build"
-	echo -e "$0 ww38 APL open-linux-driver-ci-dev_media-3084"
+	echo -e "$0 driver_build"
+	echo -e "$0 open-linux-driver-ci-dev_media-3084"
 }
 
+cp_package(){
+    for i in APL CFL KBL SKL ICL ; do
+        mkdir ./$i/$driver_build
+        cp $gta_pac_path${i}_GTAX-*-FM_task_logs*.zip ./$i/$driver_build
+    done
+}
 
 # unzip perf sw overhead data csv file from GTA download. (SW_CASE_ID_perf_summary.csv)
 get_sw_csv(){
-    
-    unzip *.zip -d ./temp && cp ./temp/*/logs/SW_*perf_summary.csv ./ && rm -rf temp
-
+    for i in APL CFL KBL SKL ICL ; do
+        unzip ./$i/$driver_build/*.zip -d ./$i/$driver_build/temp && \
+        cp ./$i/$driver_build/temp/*/logs/SW_*perf_summary.csv ./$i/$driver_build && \
+        rm -rf ./$i/$driver_build/temp
+    done
 
 }
 
 capture_sw_data(){
-    cat SW_*.csv | awk -F ',' '{print $1 "," $3}' | sed -e '/Summary/d;/CPU Latency Tag/d' > temp.csv
-    sed -i '1i CPU Latency Tag,Average (ms)' temp.csv
-
+    for i in APL CFL KBL SKL ICL ; do
+        cat ./$i/$driver_build/SW_*.csv | awk -F ',' '{print $1 "," $3}' | sed -e '/Summary/d;/CPU Latency Tag/d' > ./$i/$driver_build/temp.csv
+        sed -i '1i CPU Latency Tag,Average (ms)' ./$i/$driver_build/temp.csv
+    done
 }
 
 
@@ -61,8 +69,10 @@ done
 
 # mian funcation
 if [ $# -gt -0 ]; then
+    cp_package
 	get_sw_csv
     capture_sw_data
+    echo -e "\e[32m Done.\e[0m"
 else
 	help_info
 	exit 1
